30a6fe87ee352bd4ef4c5e8b75ccedf973f15066
yugure <109891005+yugure-orca@users.noreply.github.com>
2023-10-04 13:11:31 +0900

    add SyncAccountFetcher impl sample

---
+++ examples/essentials_sync_quote_swap.py
@@ -0,0 +1,118 @@
+# ATTENTION!
+#
+# to use this script, you need to create wallet.json
+# and it holds some USDC (>= 0.1) and SAMO (>= 1)
+#
+# solana related library:
+#   - solders   ( >= 0.9.3  )
+#   - solana    ( >= 0.27.2 )
+#   - anchorpy  ( >= 0.11.0 )
+#
+# NOTE!
+# whirlpool_essentials is in a very early stage and is subject to change, including breaking changes.
+#
+import os
+from dotenv import load_dotenv
+from decimal import Decimal
+from solana.rpc.api import Client
+from solana.publickey import PublicKey
+from solana.keypair import Keypair
+
+# ported functions from whirlpools-sdk and common-sdk
+from sync_account_fetcher import SyncAccountFetcher
+from orca_whirlpool.utils import DecimalUtil, PriceMath, SwapUtil, PDAUtil, TokenUtil
+from orca_whirlpool.constants import ORCA_WHIRLPOOL_PROGRAM_ID
+from orca_whirlpool.quote import QuoteBuilder, SwapQuoteParams
+from orca_whirlpool.types import Percentage, SwapDirection, SpecifiedAmount, TickArrayReduction
+
+load_dotenv()
+RPC_ENDPOINT_URL = os.getenv("RPC_ENDPOINT_URL")
+SAMO_USDC_WHIRLPOOL_PUBKEY = PublicKey("9vqYJjDUFecLL2xPUC4Rc7hyCtZ6iJ4mDiVZX7aFXoAe")
+
+
+def main():
+    # use dummy wallet
+    keypair = Keypair.generate();
+
+    # create Anchor client
+    connection = Client(RPC_ENDPOINT_URL)
+    # ctx
+    fetcher = SyncAccountFetcher(connection)
+    program_id = ORCA_WHIRLPOOL_PROGRAM_ID
+
+    # get whirlpool
+    whirlpool_pubkey = SAMO_USDC_WHIRLPOOL_PUBKEY
+    whirlpool = fetcher.get_whirlpool(whirlpool_pubkey)
+    token_a_decimal = (fetcher.get_token_mint(whirlpool.token_mint_a)).decimals  # SAMO_DECIMAL
+    token_b_decimal = (fetcher.get_token_mint(whirlpool.token_mint_b)).decimals  # USDC_DECIMAL
+    print("whirlpool token_mint_a", whirlpool.token_mint_a)
+    print("whirlpool token_mint_b", whirlpool.token_mint_b)
+    print("whirlpool tick_spacing", whirlpool.tick_spacing)
+    print("whirlpool tick_current_index", whirlpool.tick_current_index)
+    print("whirlpool sqrt_price", whirlpool.sqrt_price)
+    price = PriceMath.sqrt_price_x64_to_price(whirlpool.sqrt_price, token_a_decimal, token_b_decimal)
+    print("whirlpool price", DecimalUtil.to_fixed(price, token_b_decimal))
+
+    # input
+    amount = DecimalUtil.to_u64(Decimal("100"), token_b_decimal)  # USDC
+    direction = SwapDirection.BtoA
+    specified_amount = SpecifiedAmount.SwapInput
+    other_amount_threshold = 0
+    sqrt_price_limit = SwapUtil.get_default_sqrt_price_limit(direction)
+    acceptable_slippage = Percentage.from_fraction(1, 100)  # 1%
+
+    # get TickArray
+    pubkeys = SwapUtil.get_tick_array_pubkeys(
+        whirlpool.tick_current_index,
+        whirlpool.tick_spacing,
+        direction,
+        program_id,
+        whirlpool_pubkey
+    )
+    print("tickarrays", pubkeys)
+
+    # get quote
+    tick_arrays = fetcher.list_tick_arrays(pubkeys)
+    quote = QuoteBuilder.swap(SwapQuoteParams(
+        whirlpool=whirlpool,
+        amount=amount,
+        other_amount_threshold=other_amount_threshold,
+        sqrt_price_limit=sqrt_price_limit,
+        direction=direction,
+        specified_amount=specified_amount,
+        tick_arrays=tick_arrays,
+        slippage_tolerance=acceptable_slippage,
+    ), TickArrayReduction.Conservative)
+    print(quote)
+    print("amount", quote.amount)
+    print("other_amount_threshold", quote.other_amount_threshold)
+    print("slippage", acceptable_slippage)
+    print("direction", quote.direction)
+    print("specified_amount", quote.specified_amount)
+    print("tick_arrays", quote.tick_array_0, quote.tick_array_1, quote.tick_array_2)
+    print("price before trade", PriceMath.sqrt_price_x64_to_price(whirlpool.sqrt_price, token_a_decimal, token_b_decimal))
+    print("price after trade", PriceMath.sqrt_price_x64_to_price(quote.estimated_end_sqrt_price, token_a_decimal, token_b_decimal))
+
+main()
+
+"""
+SAMPLE OUTPUT:
+
+$ python essentials_quote_swap.py
+whirlpool token_mint_a 7xKXtg2CW87d97TXJSDpbD5jBkheTqA83TZRuJosgAsU
+whirlpool token_mint_b EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v
+whirlpool tick_spacing 64
+whirlpool tick_current_index -127810
+whirlpool sqrt_price 30954288334991641
+whirlpool price 0.002816
+tickarrays [ArnRmfQ49b2otrns9Kjug8fZXS8UdmKtxR2arpaevtxq, Gad6jpBXSxFmSqcPSPTE9jABp9ragNc2VsdUCNWLEAMT, 4xM1zPj8ihLFUs2DvptGVZKkdACSZgNaa8zpBTApNk9G]
+SwapQuote(amount=100000000, other_amount_threshold=34788485701121, sqrt_price_limit=79226673515401279992447579055, direction=<SwapDirection.BtoA: 'B to A'>, specified_amount=<SpecifiedAmount.SwapInput: 'Swap Input'>, tick_array_0=ArnRmfQ49b2otrns9Kjug8fZXS8UdmKtxR2arpaevtxq, tick_array_1=Gad6jpBXSxFmSqcPSPTE9jABp9ragNc2VsdUCNWLEAMT, tick_array_2=Gad6jpBXSxFmSqcPSPTE9jABp9ragNc2VsdUCNWLEAMT, estimated_amount_in=100000000, estimated_amount_out=35139884546587, estimated_end_tick_index=-127658, estimated_end_sqrt_price=31189725685457391, estimated_fee_amount=300001)
+amount 100000000
+other_amount_threshold 34788485701121
+slippage 1/100
+direction SwapDirection.BtoA
+specified_amount SpecifiedAmount.SwapInput
+tick_arrays ArnRmfQ49b2otrns9Kjug8fZXS8UdmKtxR2arpaevtxq Gad6jpBXSxFmSqcPSPTE9jABp9ragNc2VsdUCNWLEAMT Gad6jpBXSxFmSqcPSPTE9jABp9ragNc2VsdUCNWLEAMT
+price before trade 0.002815802578887143032313588563
+price after trade 0.002858799288180870045611782753
+"""
---
+++ examples/sync_account_fetcher.py
@@ -0,0 +1,102 @@
+from typing import List, Optional
+from solders.account import Account
+from solana.publickey import PublicKey
+from solana.rpc.api import Client
+from orca_whirlpool.internal.types.types import BlockTimestamp
+from orca_whirlpool.internal.accounts.types import WhirlpoolsConfig, FeeTier, Whirlpool, TickArray, Position, MintInfo, AccountInfo
+from orca_whirlpool.internal.accounts.account_parser import AccountParser
+from orca_whirlpool.internal.accounts.keyed_account_converter import KeyedAccountConverter
+
+
+BULK_FETCH_CHUNK_SIZE = 100
+
+
+# https://github.com/orca-so/whirlpools/blob/7b9ec351e2048c5504ffc8894c0ec5a9e78dc113/sdk/src/network/public/fetcher.ts
+class SyncAccountFetcher:
+    def __init__(self, connection: Client):
+        self._connection = connection
+        self._cache = {}
+
+    def _get(self, pubkey: PublicKey, parser, keyed_converter, refresh: bool):
+        key = str(pubkey)
+        if not refresh and key in self._cache:
+            return self._cache[key]
+
+        res = self._connection.get_account_info(pubkey)
+        if res.value is None:
+            return None
+
+        parsed = parser(res.value.data)
+        if parsed is None:
+            return None
+        keyed = keyed_converter(pubkey, parsed)
+
+        self._cache[key] = keyed
+        return keyed
+
+    def _list(self, pubkeys: List[PublicKey], parser, keyed_converter, refresh: bool):
+        fetch_needed = list(filter(lambda p: refresh or str(p) not in self._cache, pubkeys))
+
+        if len(fetch_needed) > 0:
+            fetched = self._bulk_fetch(fetch_needed)
+            for i in range(len(fetch_needed)):
+                if fetched[i] is None:
+                    continue
+                parsed = parser(fetched[i].data)
+                if parsed is None:
+                    continue
+                keyed = keyed_converter(fetch_needed[i], parsed)
+                self._cache[str(fetch_needed[i])] = keyed
+
+        return list(map(lambda p: self._cache.get(str(p)), pubkeys))
+
+    def _bulk_fetch(self, pubkeys: List[PublicKey]) -> List[Optional[Account]]:
+        accounts = []
+        for i in range(0, len(pubkeys), BULK_FETCH_CHUNK_SIZE):
+            chunk = pubkeys[i:(i+BULK_FETCH_CHUNK_SIZE)]
+            fetched = self._connection.get_multiple_accounts(chunk)
+            accounts.extend(fetched.value)
+        return accounts
+
+    def get_whirlpool(self, pubkey: PublicKey, refresh: bool = False) -> Optional[Whirlpool]:
+        return self._get(pubkey, AccountParser.parse_whirlpool, KeyedAccountConverter.to_keyed_whirlpool, refresh)
+
+    def get_whirlpools_config(self, pubkey: PublicKey, refresh: bool = False) -> Optional[WhirlpoolsConfig]:
+        return self._get(pubkey, AccountParser.parse_whirlpools_config, KeyedAccountConverter.to_keyed_whirlpools_config, refresh)
+
+    def get_fee_tier(self, pubkey: PublicKey, refresh: bool = False) -> Optional[FeeTier]:
+        return self._get(pubkey, AccountParser.parse_fee_tier, KeyedAccountConverter.to_keyed_fee_tier, refresh)
+
+    def get_position(self, pubkey: PublicKey, refresh: bool = False) -> Optional[Position]:
+        return self._get(pubkey, AccountParser.parse_position, KeyedAccountConverter.to_keyed_position, refresh)
+
+    def get_tick_array(self, pubkey: PublicKey, refresh: bool = False) -> Optional[TickArray]:
+        return self._get(pubkey, AccountParser.parse_tick_array, KeyedAccountConverter.to_keyed_tick_array, refresh)
+
+    def get_token_account(self, pubkey: PublicKey, refresh: bool = False) -> Optional[AccountInfo]:
+        return self._get(pubkey, AccountParser.parse_token_account, KeyedAccountConverter.to_keyed_token_account, refresh)
+
+    def get_token_mint(self, pubkey: PublicKey, refresh: bool = False) -> Optional[MintInfo]:
+        return self._get(pubkey, AccountParser.parse_token_mint, KeyedAccountConverter.to_keyed_token_mint, refresh)
+
+    def list_whirlpools(self, pubkeys: List[PublicKey], refresh: bool = False) -> List[Optional[Whirlpool]]:
+        return self._list(pubkeys, AccountParser.parse_whirlpool, KeyedAccountConverter.to_keyed_whirlpool, refresh)
+
+    def list_positions(self, pubkeys: List[PublicKey], refresh: bool = False) -> List[Optional[Position]]:
+        return self._list(pubkeys, AccountParser.parse_position, KeyedAccountConverter.to_keyed_position, refresh)
+
+    def list_tick_arrays(self, pubkeys: List[PublicKey], refresh: bool = False) -> List[Optional[TickArray]]:
+        return self._list(pubkeys, AccountParser.parse_tick_array, KeyedAccountConverter.to_keyed_tick_array, refresh)
+
+    def list_token_accounts(self, pubkeys: List[PublicKey], refresh: bool = False) -> List[Optional[AccountInfo]]:
+        return self._list(pubkeys, AccountParser.parse_token_account, KeyedAccountConverter.to_keyed_token_account, refresh)
+
+    def list_token_mints(self, pubkeys: List[PublicKey], refresh: bool = False) -> List[Optional[MintInfo]]:
+        return self._list(pubkeys, AccountParser.parse_token_mint, KeyedAccountConverter.to_keyed_token_mint, refresh)
+
+    def get_latest_block_timestamp(self) -> BlockTimestamp:
+        res1 = self._connection.get_latest_blockhash()
+        slot = res1.context.slot
+        res2 = self._connection.get_block_time(slot)
+        timestamp = res2.value
+        return BlockTimestamp(slot=slot, timestamp=timestamp)
